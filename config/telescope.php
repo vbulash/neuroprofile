eaef137b2034c8768d04ee3a25600dd46d78420e
Валерий Булаш <vbulash@yandex.ru>
2023-06-08 09:34:44 +0300

    Перенос аудита изменений БД из Платформы 1 Fixes #16

--- config/telescope.php
+++ config/telescope.php
@@ -6,41 +6,41 @@ use Laravel\Telescope\Watchers;
 return [
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Domain
-	|--------------------------------------------------------------------------
-	|
-	| This is the subdomain where Telescope will be accessible from. If the
-	| setting is null, Telescope will reside under the same domain as the
-	| application. Otherwise, this value will be used as the subdomain.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Domain
+	   |--------------------------------------------------------------------------
+	   |
+	   | This is the subdomain where Telescope will be accessible from. If the
+	   | setting is null, Telescope will reside under the same domain as the
+	   | application. Otherwise, this value will be used as the subdomain.
+	   |
+	   */
 
 	'domain' => env('TELESCOPE_DOMAIN'),
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Path
-	|--------------------------------------------------------------------------
-	|
-	| This is the URI path where Telescope will be accessible from. Feel free
-	| to change this path to anything you like. Note that the URI will not
-	| affect the paths of its internal API that aren't exposed to users.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Path
+	   |--------------------------------------------------------------------------
+	   |
+	   | This is the URI path where Telescope will be accessible from. Feel free
+	   | to change this path to anything you like. Note that the URI will not
+	   | affect the paths of its internal API that aren't exposed to users.
+	   |
+	   */
 
 	'path' => env('TELESCOPE_PATH', 'telescope'),
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Storage Driver
-	|--------------------------------------------------------------------------
-	|
-	| This configuration options determines the storage driver that will
-	| be used to store Telescope's data. In addition, you may set any
-	| custom options as needed by the particular driver you choose.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Storage Driver
+	   |--------------------------------------------------------------------------
+	   |
+	   | This configuration options determines the storage driver that will
+	   | be used to store Telescope's data. In addition, you may set any
+	   | custom options as needed by the particular driver you choose.
+	   |
+	   */
 
 	'driver' => env('TELESCOPE_DRIVER', 'database'),
 
@@ -52,28 +52,28 @@ return [
 	],
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Master Switch
-	|--------------------------------------------------------------------------
-	|
-	| This option may be used to disable all Telescope watchers regardless
-	| of their individual configuration, which simply provides a single
-	| and convenient way to enable or disable Telescope data storage.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Master Switch
+	   |--------------------------------------------------------------------------
+	   |
+	   | This option may be used to disable all Telescope watchers regardless
+	   | of their individual configuration, which simply provides a single
+	   | and convenient way to enable or disable Telescope data storage.
+	   |
+	   */
 
 	'enabled' => env('TELESCOPE_ENABLED', true),
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Route Middleware
-	|--------------------------------------------------------------------------
-	|
-	| These middleware will be assigned to every Telescope route, giving you
-	| the chance to add your own middleware to this list or change any of
-	| the existing middleware. Or, you can simply stick with this list.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Route Middleware
+	   |--------------------------------------------------------------------------
+	   |
+	   | These middleware will be assigned to every Telescope route, giving you
+	   | the chance to add your own middleware to this list or change any of
+	   | the existing middleware. Or, you can simply stick with this list.
+	   |
+	   */
 
 	'middleware' => [
 		'web',
@@ -81,15 +81,15 @@ return [
 	],
 
 	/*
-	|--------------------------------------------------------------------------
-	| Allowed / Ignored Paths & Commands
-	|--------------------------------------------------------------------------
-	|
-	| The following array lists the URI paths and Artisan commands that will
-	| not be watched by Telescope. In addition to this list, some Laravel
-	| commands, like migrations and queue commands, are always ignored.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Allowed / Ignored Paths & Commands
+	   |--------------------------------------------------------------------------
+	   |
+	   | The following array lists the URI paths and Artisan commands that will
+	   | not be watched by Telescope. In addition to this list, some Laravel
+	   | commands, like migrations and queue commands, are always ignored.
+	   |
+	   */
 
 	'only_paths' => [
 		// 'api/*'
@@ -104,15 +104,15 @@ return [
 	],
 
 	/*
-	|--------------------------------------------------------------------------
-	| Telescope Watchers
-	|--------------------------------------------------------------------------
-	|
-	| The following array lists the "watchers" that will be registered with
-	| Telescope. The watchers gather the application's profile data when
-	| a request or task is executed. Feel free to customize this list.
-	|
-	*/
+	   |--------------------------------------------------------------------------
+	   | Telescope Watchers
+	   |--------------------------------------------------------------------------
+	   |
+	   | The following array lists the "watchers" that will be registered with
+	   | Telescope. The watchers gather the application's profile data when
+	   | a request or task is executed. Feel free to customize this list.
+	   |
+	   */
 
 	'watchers' => [
 		Watchers\BatchWatcher::class => env('TELESCOPE_BATCH_WATCHER', true),
@@ -159,7 +159,7 @@ return [
 
 		Watchers\ModelWatcher::class => [
 			'enabled' => env('TELESCOPE_MODEL_WATCHER', true),
-			'events' => ['eloquent.*'],
+			'events' => ['eloquent.created*', 'eloquent.updated*', 'eloquent.deleted*'],
 			'hydrations' => true,
 		],
 
